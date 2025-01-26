ebf24d60ee42f2e04ba22e957e456c10e43162d3
iamshirazi <smatthew120@gmail.com>
2025-01-25 22:33:18 -0500

    Updated CanadianElection2019.py to generate an HTML version of the electoral map. The HTML map is far superior in terms of user experience, performance, and attractiveness. Added Dockerfile for docker compatibility. Added requirements.txt file to easily download all necessary libraries. Updated README.

--- CanadianElection2019.py
+++ CanadianElection2019.py
@@ -1,7 +1,9 @@
 # Copyright (c) 2023 Matthew Shirazi
 
 import geopandas as gpd
+from matplotlib.colors import LinearSegmentedColormap
 import matplotlib.pyplot as plt
+import pandas as pd
 
 # COLOURS
 Lib = '#EE3224'  # (238, 50, 36)
@@ -11,16 +13,22 @@ Green = '#3D9B35'  # (61, 155, 53)
 BQ = '#00A7EC'  # (0, 167, 236)
 Ind = '#847e7e'
 
-# read file
+# read shapefile
 districts = gpd.read_file("districts2/lfed000b16a_e.shp")
+votes = pd.read_csv("voting_data/Canada2019.txt", sep=" ", header=0)
+
+# Merge district shapes and number of votes
+districts['FEDUID'] = districts['FEDUID'].astype(int)
+districts = districts.merge(votes, on='FEDUID')
 
 colour = []
+win = []
 
-# read file with results
+# read file with voting results
 with open('voting_data/Canada2019.txt') as file:
 
     for _ in range(1):
-        next(file)  # SKIp FIRST LINE
+        next(file)  # SKIP FIRST LINE
 
     for line in file:
         # Removes new line symbol and splits the votes
@@ -30,34 +38,50 @@ with open('voting_data/Canada2019.txt') as file:
 
         if len(line) != 0:
             results = [int(line[2]), int(line[3]), int(
-                line[4]), int(line[5]), int(line[6])]
+                line[4]), int(line[5]), int(line[6]), int(line[7])]
 
         winner = 0
 
-        for i in range(0, 5):
+        for i in range(0, 6):
             if results[i] > winner:
                 winner = int(results[i])
 
         if winner == int(results[0]):
             colour.append(Lib)
+            win.append("Liberal")
         elif winner == int(results[1]):
             colour.append(Con)
+            win.append("Conservative")
         elif winner == int(results[2]):
             colour.append(NDP)
+            win.append("NDP")
         elif winner == int(results[3]):
             colour.append(Green)
+            win.append("Green")
         elif winner == int(results[4]):
             colour.append(BQ)
-        elif winner == 0:
+            win.append("Bloc Quebecois")
+        elif winner == int(results[5]):
             colour.append(Ind)
+            win.append("Independent")
 
+
+# Sort the ridings
+ridings = districts.sort_values('FEDUID')
 
 # Colour the ridings
-districts['color'] = colour
+ridings["Party"] = win
+ridings['color'] = colour
 
-# plot the coloured ridings
-districts.sort_values('FEDUID').plot(color=districts['color'])
+m = ridings.explore(
+    column="Party", # make choropleth vased on winner in column
+    tooltip=["Riding", "Lib", "Con", "NDP", "Green", "BQ", "Ind"], # show all party votes for a riding when hovering over it
+    popup=True, # show all values of a riding when you click it
+    tiles="CartoDB positron", # use "CartoDB positron" tiles
+    cmap=['#00A7EC', '#0F2D52', '#3D9B35', '#847e7e', '#EE3224', '#F58220'],
+    style_kwds=dict(color="black"), #use black outline
+)
 
-# show map
-plt.axis('off')
-plt.show()
+outfp = r"./election2019.html"
+
+m.save(outfp)
--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # Canadian Elections
 
 ## Description:
-### This project uses Python to draw Canada and all of its official ridings, then colours them based on which party won in that riding.
-### The map of Canada is highly detailed, and allows you to zoom into any part of the country.
-### Will add previous and future elections to this project.
+This project uses Python to draw Canada and all of its official ridings, then colours them based on which party won in that riding.
 
-## How to run project:
-### Clone this repository then run CanadianElections2019.py.
+The map of Canada is highly detailed, and allows you to zoom into any part of the country seamlessly.
+
+Will add previous and future elections to this project.
+Will also improve performance so zooming in/out of the map doesn't have as much lag.
+
+## How to run this project:
+1. Clone this repository
+2. Run: `pip install -r requirements.txt` to install all necessary libraries
+3. Run CanadianElection2019.py
+4. Copy the path of the election2019.html file
+5. Paste the path into your browser to view the electoral map
