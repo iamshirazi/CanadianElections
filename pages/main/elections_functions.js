96d786bdd79963ece2c84b602d1d8529dca37619
iamshirazi <smatthew120@gmail.com>
2025-10-04 19:25:17 -0400

    Updated sitemap and size of the parliament chart seats

--- pages/main/elections_functions.js
+++ pages/main/elections_functions.js
@@ -3,11 +3,13 @@ function loadElection(electionYear) {
     document.getElementById('iframe').src = "/elections/election" + electionYear + ".html";
     document.getElementById('current_election_text').innerHTML = electionYear
     set_parliament_chart(electionYear)
+    display_parliament_chart_button()
 };
 
 function loadAbout() {
     document.getElementById('iframe').src = "/main/about.html";
     document.getElementById('current_election_text').innerHTML = ""
+    hide_parliament_chart_button()
 };
 
 function set_parliament_chart(electionYear) {
@@ -33,4 +35,18 @@ function toggle_parliament_chart() {
     } else {
         parl_chart.style.display = "none";
     }
+}
+
+function display_parliament_chart_button() {
+    const parl_chart_button = document.getElementById('parliamentChartButton')
+
+    if (parl_chart_button.style.display != "flex") {
+        parl_chart_button.style.display = "flex";
+    }
+}
+
+function hide_parliament_chart_button() {
+    const parl_chart_button = document.getElementById('parliamentChartButton')
+
+    parl_chart_button.style.display = "none";
 }
