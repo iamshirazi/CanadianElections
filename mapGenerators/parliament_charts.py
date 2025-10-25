3fd0636c15ce6cbb2ef8e13f195b7e103dc19e42
iamshirazi <smatthew120@gmail.com>
2025-10-24 22:34:51 -0400

    Added 1925 election, updated pull_voting_data_updated.py. Fixed error in 1921 voting data.

--- mapGenerators/parliament_charts.py
+++ mapGenerators/parliament_charts.py
@@ -267,6 +267,24 @@ def create_parliament_seating_plan_1921(con_seats, lib_seats, progressive_seats,
 
     return parliament_seats
 
+def create_parliament_seating_plan_1925(con_seats, lib_seats, progressive_seats, uf_alberta_seats, labour_seats, independent_seats):
+    parliament_seats = []
+
+    for i in range(lib_seats):
+        parliament_seats.append('Liberal')
+    for i in range(con_seats):
+        parliament_seats.append('Conservative')
+    for i in range(progressive_seats):
+        parliament_seats.append('Progressive')
+    for i in range(independent_seats):
+        parliament_seats.append('Independent')
+    for i in range(labour_seats):
+        parliament_seats.append('Labour')
+    for i in range(uf_alberta_seats):
+        parliament_seats.append('United-Farmers-of-Alberta')
+
+    return parliament_seats
+
 def create_parliament_seating_plan_2019(con_seats, lib_seats, ndp_seats, green_seats, bloq_seats, independent_seats):
     parliament_seats = []
 
