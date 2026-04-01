5272e56a54190a3b813c5da2aae4981f46758eea
iamshirazi <smatthew120@gmail.com>
2026-04-01 19:57:50 -0400

    Reverting database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -6,9 +6,10 @@ import os
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 POSTGRES_DB = os.getenv("POSTGRES_DB")
-POSTGRES_URL = os.getenv("POSTGRES_URL")
 
-engine = create_engine(POSTGRES_URL)
+DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@postgres-service:5432/{POSTGRES_DB}"
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
